5f93a85a05cd0ad44a07715429fb62a4af9bd810
Viktor Daňko <viktor.danko@superkoders.com>
2020-10-18 17:51:52 +0200

    Add header with logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
+import { RowMain } from "./Layout/RowMain";
+import { HeaderWrap } from "@components/HeaderWrap";
+import { Logo } from "./Logo";
+
 export default function Header() {
 	return (
-		<h1>
-			Welcome to the jungle!
-		</h1>
+		<header className="header">
+			<RowMain>
+				<HeaderWrap>
+					<Logo />
+				</HeaderWrap>
+			</RowMain>
+		</header>
 	)
 }
