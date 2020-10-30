32ee16e1c3a0d71cc473284d656b572805b463e9
Viktor Daňko <viktor.danko@superkoders.com>
2020-10-30 18:20:37 +0100

    RWD | Mobile menu

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,29 @@
+import { useState, useRef } from 'react';
+import classNames from 'classnames';
 import { RowMain } from "./Layout/RowMain";
 import { HeaderWrap } from "components/HeaderWrap";
 import { Logo } from "./Logo";
 import { Menu } from "./Menu/Menu";
 import { MenuList } from "./Menu/MenuList";
+import { MenuButton } from "./Menu/MenuButton";
+import { useOnClickOutside } from "../hooks/useOnClickOutside";
 
 export default function Header() {
+
+	const [menuOpened, setMenuOpened] = useState(false);
+	const className = classNames('header', menuOpened && 'is-opened');
+
+	const header = useRef(null);
+	useOnClickOutside(header, () => setMenuOpened(false));
+
 	return (
-		<header className="header">
+		<header className={className} ref={header}>
 			<RowMain>
 				<HeaderWrap>
 					<Logo />
 					<Menu>
 						<MenuList />
+						<MenuButton menuOpened={menuOpened} setMenuOpened={setMenuOpened} />
 					</Menu>
 				</HeaderWrap>
 			</RowMain>
---
+++ components/Menu/MenuButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export const MenuButton = ({ menuOpened, setMenuOpened }) => {
+  return (
+    <button className="m-mobile-menu btn" data-text="Menu" aria-labelledby="mobileMenu" aria-expanded={menuOpened ? 'true' : 'false'} onClick={() => setMenuOpened(!menuOpened)}>
+      <span className="m-mobile-menu__icon"></span>
+      <span className="u-vhide" id="mobileMenu">Mobilní menu</span>
+    </button>
+  );
+};
