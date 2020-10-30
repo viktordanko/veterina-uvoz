f4c320681d41ec0763bfd1a55ffe952b45d9c8a8
Viktor Daňko <viktor.danko@superkoders.com>
2020-10-30 18:20:46 +0100

    RWD | Intro

--- components/Intro.js
+++ components/Intro.js
@@ -1,20 +1,20 @@
 import { RowMain } from "./Layout/RowMain"
+import { Text } from "./Text"
 
 export const Intro = () => {
   return (
     <div className="b-intro">
-      <div className="b-intro__inner">
-        <RowMain>
-          <div className="b-intro__content">
-            <h1>
-              Veterina V.I.Pet
+      <RowMain>
+        <div className="b-intro__inner"></div>
+        <div className="b-intro__content">
+          <h1>
+            Veterina V.I.Pet
               <br />
               Very Important Pet
-            </h1>
-            <p>...protože každý mazlíček je důležitý</p>
-          </div>
-        </RowMain>
-      </div>
+          </h1>
+          <Text isBold>...protože každý mazlíček je důležitý</Text>
+        </div>
+      </RowMain>
     </div>
   );
 };
--- components/Menu/MenuList.js
+++ components/Menu/MenuList.js
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import { MenuLink } from './MenuLink';
 
 const menuItems = [
