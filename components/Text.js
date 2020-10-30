428b52303b065cb5eaca4acd8f25e1f4784bf083
Viktor Daňko <viktor.danko@superkoders.com>
2020-10-30 12:31:42 +0100

    Text component can be bold

--- components/Text.js
+++ components/Text.js
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 
 
-export const Text = ({ children, horizontalAlign, color, size }) => {
+export const Text = ({ children, horizontalAlign, color, size, isBold }) => {
   const className = classNames(
     horizontalAlign == 'center'
       ? 'u-text-center'
@@ -9,7 +9,8 @@ export const Text = ({ children, horizontalAlign, color, size }) => {
         ? 'u-text-right'
         : undefined,
     color ? `u-color-${color}` : false,
-    size ? `u-font-${size}` : false
+    size ? `u-font-${size}` : false,
+    isBold ? 'u-font-bold' : false
   );
 
   return <p className={className}>{children}</p>;
