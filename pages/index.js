68474b05f210ef78dbb6e98e34020194eeee0cb1
Viktor Daňko <viktor.danko@superkoders.com>
2020-10-31 08:16:49 +0100

    Add favicons

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,40 @@ import Head from 'next/head'
 import { Contact } from '@components/Contact'
 
 
-export default function Home({ children }) {
+export default function Home() {
   return (
     <>
       <Head>
-        <title>Next Veterina Úvoz!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Veterina V.I.Pet | MVDr. Klára Palicová</title>
+        <meta
+          key="description"
+          name="description"
+          content="Veterinární ordinace V.I.Pet na Úvoze, Brno"
+        />
+        <meta key="og:type" property="og:type" content="website" />
+        <meta
+          key="og:title"
+          property="og:title"
+          content="Veterinární ordinace V.I.Pet na Úvoze, Brno"
+        />
+        <meta
+          key="og:description"
+          property="og:description"
+          content="...protože každý mazlíček je důležitý"
+        />
+        <meta
+          key="og:image"
+          property="og:image"
+          content="/og-vipet.png"
+        />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Header />
       <Main>
