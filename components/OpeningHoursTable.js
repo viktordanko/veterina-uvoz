3a914f55084cce48f149bd502c486d6e28255930
Viktor Danko <vik@panter.ch>
2021-10-17 13:38:46 +0200

    Office hours i18n

--- components/OpeningHoursTable.js
+++ components/OpeningHoursTable.js
@@ -1,38 +1,53 @@
 import classNames from 'classnames';
-import days from '../api/opening-hours.json';
+import { useTranslation } from 'react-i18next';
 import { PawDivider } from './PawDivider';
 
 
 export const OpeningHoursTable = () => {
   const classes = classNames('b-opening');
+  const { t } = useTranslation();
 
   return (
     <div className={classes}>
-      <PawDivider heading="Ordinační hodiny" />
+      <PawDivider heading={t('openHours.title')} />
       {/* <p class="u-text-center u-color-red">
         V termínu <strong>7. 8 - 14. 8. 2021</strong> bude ordinace zavřená.
       </p> */}
       <table className="b-opening__table">
         <tbody>
-          {days.map((day, index) => {
-            const { name, from, to, colSpan } = day;
-
-            if (colSpan) {
-              return (
-                <tr key={index}>
-                  <th>{name}</th>
-                  <td colSpan={colSpan}>{from}</td>
-                </tr>
-              )
-            }
-            return (
-              <tr key={index}>
-                <th>{name}</th>
-                <td colSpan={colSpan}>{from}</td>
-                <td>{to}</td>
-              </tr>
-            )
-          })}
+          <tr>
+            <th>{t('openHours.monday')}</th>
+            <td >9:00 - 11:00</td>
+            <td>15:00 - 20:00</td>
+          </tr>
+          <tr>
+            <th>{t('openHours.tuesday')}</th>
+            <td>9:00 - 11:00</td>
+            <td>15:00 - 20:00</td>
+          </tr>
+          <tr>
+            <th>{t('openHours.wednesday')}</th>
+            <td colSpan="2">{t('openHours.booked')}</td>
+          </tr>
+          <tr>
+            <th>{t('openHours.thursday')}</th>
+            <td colSpan="2">{t('openHours.booked')}</td>
+          </tr>
+          <tr>
+            <th>{t('openHours.friday')}</th>
+            <td>9:00 - 11:00</td>
+            <td>19:00 - 20:00</td>
+          </tr>
+          <tr>
+            <th>{t('openHours.saturday')}</th>
+            <td>10:00 - 11:30</td>
+            <td>15:00 - 18:30</td>
+          </tr>
+          <tr>
+            <th>{t('openHours.sunday')}</th>
+            <td>10:00 - 11:30</td>
+            <td>15:00 - 18:30</td>
+          </tr>
         </tbody>
       </table>
     </div>
