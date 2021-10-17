90a112b5d7f95d7b3dfb263de0277e336530537c
Viktor Danko <vik@panter.ch>
2021-10-17 13:23:39 +0200

    About us i18n

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -4,32 +4,25 @@ import { AboutUsSection } from './sections/AboutUsSection';
 import { MaxWidth } from './Layout/MaxWidth';
 import { Text } from './Text';
 import { Annot } from './Annot';
+import { useTranslation } from 'react-i18next';
 
 
 export const AboutUs = () => {
+  const { t } = useTranslation();
   return (
     <RowMain>
       <AboutUsSection id="about-us">
-        <PawDivider heading="Vítejte!" />
+        <PawDivider heading={t('about.welcome')} />
         <MaxWidth isCenter variant="10-12">
           <Annot spacing="xxxl">
             <Text>
-              Jmenuji se <strong>Klára Palicová</strong> a můj pes je pro mě v
-              životě mým nepostradatelným parťákem a&nbsp;miláčkem. Od&nbsp;roku 2013 se věnuji veterinární medicíně zejména{' '}
-              <strong>psů a koček</strong>. V profesním životě jsem pracovala
-              převážně na NON-STOP klinikách, kde jsem viděla a ošetřila široké spektrum případů.
+              {t('about.first')}
             </Text>
             <Text>
-              Své vědomosti a zkušenosti využiji při léčbě Vašich čtyřnohých
-              společníků. Budu se jim věnovat s péčí, jakoby byli{' '}
-              <strong>&#8222;moji vlastní&#8220;</strong> v příznivé otevírací
-              době a to i víkendech.
+              {t('about.second')}
             </Text>
             <Text>
-              Problémy Vašich mazlíčků budeme řešit společně a v případě
-              nutnosti úzce-specializovaného vyšetření zajistím objednání ke
-              specialistovi, k co možná nejlepší spokojenosti Vás i Vašich
-              miláčků &ndash; protože o ně tady kráčí&hellip;
+              {t('about.third')}
             </Text>
           </Annot>
         </MaxWidth>
