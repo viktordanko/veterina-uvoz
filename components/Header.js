53ccc954d02e165184b2a4bdf15e1ff5ebb68990
Viktor Danko <vik@panter.ch>
2021-10-17 13:05:50 +0200

    Header i18n

--- components/Header.js
+++ components/Header.js
@@ -6,8 +6,8 @@ import { HeaderWrap } from "components/HeaderWrap";
 import { Logo } from "./Logo";
 import { Menu } from "./Menu/Menu";
 import { MenuButton } from "./Menu/MenuButton";
-import menuItems from '../api/menu-items.json';
 import { LocaleSwitcher } from './LocaleSwitcher';
+import { useTranslation } from 'react-i18next';
 
 
 export const Header = () => {
@@ -29,6 +29,7 @@ export const Header = () => {
 	}, [])
 
 	const classes = classNames('header', menuOpened && 'is-opened');
+	const { t } = useTranslation();
 
 	return (
 		<header className={classes}>
@@ -37,7 +38,44 @@ export const Header = () => {
 					<Logo onClick={closeMenu} />
 					<Menu>
 						<ul className="m-main__list">
-							{menuItems.map(({ label, href, isPhoneButton }, id) => {
+							<li className="m-main__item">
+								<Link href="/" passHref>
+									<a className="m-main__link" onClick={closeMenu}>
+										{t('header.menuItems.intro')}
+									</a>
+								</Link>
+							</li>
+							<li className="m-main__item">
+								<Link href="#about-us" passHref>
+									<a className="m-main__link">
+										{t('header.menuItems.aboutUs')}
+									</a>
+								</Link>
+							</li>
+							<li className="m-main__item">
+								<Link href="#open-hours" passHref>
+									<a className="m-main__link">
+										{t('header.menuItems.openingHours')}
+									</a>
+								</Link>
+							</li>
+							<li className="m-main__item">
+								<Link href="#contact" passHref>
+									<a className="m-main__link">
+										{t('header.menuItems.contact')}
+									</a>
+								</Link>
+							</li>
+							<li className="m-main__item m-main__item--phone">
+								<Link href="tel:+420737112300">
+									<a className="btn btn--sm">
+										<span className="btn__text">
+											{t('header.menuItems.callUs')}
+										</span>
+									</a>
+								</Link>
+							</li>
+							{/* {menuItems.map(({ label, href, isPhoneButton }, id) => {
 								if (isPhoneButton) {
 									return (
 										<li className="m-main__item m-main__item--phone" key={id}>
@@ -69,7 +107,7 @@ export const Header = () => {
 										</Link>
 									</li>
 								);
-							})}
+							})} */}
 							<li className="m-main__item">
 								<LocaleSwitcher />
 							</li>
