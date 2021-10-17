949b882641dfa34bb241107ef5b15f65f1378f17
Viktor Danko <vik@panter.ch>
2021-10-17 13:47:32 +0200

    Why us i18n

--- components/WhyUs.js
+++ components/WhyUs.js
@@ -7,35 +7,37 @@ import { Benefits } from './Benefits';
 import { TimeClock } from './Icons/TimeClock';
 import { HourGlass } from './Icons/HourGlass';
 import { Vet } from './Icons/Vet';
+import { useTranslation } from 'react-i18next';
 
 
 export const WhyUs = () => {
+  const { t } = useTranslation();
   return (
     <RowMain>
       <WhyUsSection>
         <Benefits spacing="xxl">
-          <PawDivider heading="Proč k nám?" />
+          <PawDivider heading={t('features.title')} />
           <List isGrid variant="center" spacingX="lg" spacingY="lg">
             <ListItem
               isGridCell
               center
               mdSize={3}
               listIcon={<TimeClock size={50} />}
-              description="Jste pracující a nestíháte se&nbsp;svým&nbsp;miláčkem zajít k&nbsp;veterináři?"
+              description={t('features.first')}
             />
             <ListItem
               isGridCell
               center
               mdSize={3}
               listIcon={<HourGlass size={50} />}
-              description="Máte pocit, že Váš mazlíček je pouze další v&nbsp;řadě a&nbsp;veterinář na&nbsp;Vás nemá dostatek času?"
+              description={t('features.second')}
             />
             <ListItem
               isGridCell
               center
               mdSize={3}
               listIcon={<Vet size={50} />}
-              description="Nemáte rádi, když Vašeho miláčka pokaždé ošetřuje někdo&nbsp;jiný?​"
+              description={t('features.third')}
             />
           </List>
         </Benefits>
