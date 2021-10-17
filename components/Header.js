0cdd714a062d153212fb98c11fc1aa958c2fb090
Viktor Danko <vik@panter.ch>
2021-10-17 13:06:59 +0200

    Rm dead code

--- components/Header.js
+++ components/Header.js
@@ -75,39 +75,6 @@ export const Header = () => {
 									</a>
 								</Link>
 							</li>
-							{/* {menuItems.map(({ label, href, isPhoneButton }, id) => {
-								if (isPhoneButton) {
-									return (
-										<li className="m-main__item m-main__item--phone" key={id}>
-											<Link href={href} passHref>
-												<a className="btn btn--sm">
-													<span className="btn__text">{label}</span>
-												</a>
-											</Link>
-										</li>
-									);
-								}
-								if (href === '/') {
-									return (
-										<li className="m-main__item" key={id}>
-											<Link href={href} passHref replace>
-												<a className="m-main__link" onClick={closeMenu}>
-													{label}
-												</a>
-											</Link>
-										</li>
-									);
-								}
-								return (
-									<li className="m-main__item" key={id}>
-										<Link href={href} passHref >
-											<a className="m-main__link" onClick={closeMenu}>
-												{label}
-											</a>
-										</Link>
-									</li>
-								);
-							})} */}
 							<li className="m-main__item">
 								<LocaleSwitcher />
 							</li>
